665ed14abaa8e18df3be025418d0d52522ba3301
GUILHERME BANDOCH <68199@PROWAY.TREINA>
2020-09-05 17:45:57 -0300

    5 - Normas de escrita

---
+++ src/br/com/fundamentos/NomeandoVariaveis.java
@@ -0,0 +1,29 @@
+package src.br.com.fundamentos;
+
+public class NomeandoVariaveis {
+	public static void main(String[] args) {
+		// valido separando por inderline _
+		String variavel_separada_com_underline;
+		
+		//padrao camelCase
+		String variavelSeparadaComUnderline;
+		
+		// válido porem estranho
+		int _numero = 1;
+		
+		System.out.println(_numero);
+		
+		// válido porem mais estranho ainda! parece PHP
+		int $outroNumero = 1;
+		System.out.println($outroNumero);
+		
+		// não pode 
+		
+		//String 1um;
+		
+		//int @arroba;
+		
+		//String *&texto;
+	}
+
+}
