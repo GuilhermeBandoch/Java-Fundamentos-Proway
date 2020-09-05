1cef3242cad46ef74c69681a7760e89259298172
GUILHERME BANDOCH <68199@PROWAY.TREINA>
2020-09-05 17:19:47 -0300

    4 - Longos e float

--- src/br/com/fundamentos/TrabalhandoComVariaveis.java
+++ src/br/com/fundamentos/TrabalhandoComVariaveis.java
@@ -9,6 +9,20 @@ public class TrabalhandoComVariaveis {
 		
 		quantidade = 15;
 		
-		System.out.println(quantidade);  
+		System.out.println(quantidade);
+		
+		double numeroComVirgula = 1.3;
+		
+		System.out.println("Numero descimal ->" + numeroComVirgula);
+		
+		float numeroComVirgulaPontoFlutuante = 1.5f;
+		System.out.println("Numero decimal com float ->" + numeroComVirgulaPontoFlutuante);
+		
+		//https://medium.com/@mauriciogeneroso/java-oca-1z0-808-2-1-tipos-de-dados-vari%C3%A1veis-vari%C3%A1veis-primitivas-e-de-refer%C3%AAncia-c8f007756bc5
+		
+		long numeroInteiroGrande = 25l;
+		
+		System.out.println("Numero interios com long ->" + numeroInteiroGrande);
 	}
+	
 }
