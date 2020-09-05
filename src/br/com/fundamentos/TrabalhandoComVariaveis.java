aba8001ab68722ed3fa422174c10eba06b43c0c8
GUILHERME BANDOCH <68199@PROWAY.TREINA>
2020-09-05 16:41:37 -0300

    3 - trabalhando com variaveis

---
+++ src/br/com/fundamentos/TrabalhandoComVariaveis.java
@@ -0,0 +1,14 @@
+package src.br.com.fundamentos;
+
+public class TrabalhandoComVariaveis {
+	public static void main(String[] args) {
+		int quantidade; // Declarando variavel interio
+		quantidade = 10; // atribuindo o valor 10
+		
+		System.out.println(quantidade);
+		
+		quantidade = 15;
+		
+		System.out.println(quantidade);  
+	}
+}
